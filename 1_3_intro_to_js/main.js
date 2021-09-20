79ad0a8459d8d57649f04529cd0bec2275f985d4
Tomiko Karino <tomiko.student@gmail.com>
2021-09-20 16:33:19 -0400

    Assignment 3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+"use strict";
+
+        let clicks = 0;
+
+        function countClicks() {
+          clicks += 1;
+          document.getElementById("display").innerHTML = clicks;
+        };
+
+        function userName() {
+            let x = 
+                document.getElementById("userName").value;
+            
+            document.getElementById(
+              "displayName").innerHTML = x;
+        }
